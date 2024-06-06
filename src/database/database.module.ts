6d8f3ea6096debc17581f3219dfdb9efa8fc3872
carlosallexandre <carlos_costarodrigues@outlook.com>
2024-06-06 18:53:36 +0000

    feat: add database/config modules

---
+++ src/database/database.module.ts
@@ -0,0 +1,44 @@
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CONFIG_SCHEMA_TYPE } from 'src/config/config.module';
+
+const models = [CategoryModel];
+
+@Module({
+  imports: [
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory(configService: ConfigService<CONFIG_SCHEMA_TYPE>) {
+        const dbVendor = configService.get('DB_VENDOR');
+
+        switch (dbVendor) {
+          case 'sqlite':
+            return {
+              dialect: 'sqlite',
+              host: configService.get('DB_HOST'),
+              models,
+              logging: configService.get('DB_LOGGING'),
+              autoLoadModels: configService.get('DB_AUTO_LOAD_MODELS'),
+            };
+          case 'mysql':
+            return {
+              dialect: 'mysql',
+              host: configService.get('DB_HOST'),
+              port: configService.get('DB_PORT'),
+              database: configService.get('DB_DATABASE'),
+              username: configService.get('DB_USERNAME'),
+              password: configService.get('DB_PASSWORD'),
+              models,
+              logging: configService.get('DB_LOGGING'),
+              autoLoadModels: configService.get('DB_AUTO_LOAD_MODELS'),
+            };
+          default:
+            throw new Error(`Unsupported database configuration: ${dbVendor}`);
+        }
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
