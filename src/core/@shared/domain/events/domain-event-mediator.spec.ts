fc306bffaf18216a0ef99f855e42244306560f99
carlosallexandre <carlos_costarodrigues@outlook.com>
2024-06-28 12:29:38 +0000

    chore: add domain event mediator

---
+++ src/core/@shared/domain/events/domain-event-mediator.spec.ts
@@ -0,0 +1,54 @@
+import { AggregateRoot } from '../aggregate-root';
+import { Uuid } from '../value-objects/uuid.vo';
+import { DomainEventMediator } from './domain-event-mediator';
+import { IDomainEvent } from './domain-event.interface';
+
+class StubEvent implements IDomainEvent {
+  occurred_on: Date;
+  event_version: number;
+
+  constructor(
+    public aggregate_id: Uuid,
+    public name: string,
+  ) {
+    this.occurred_on = new Date();
+    this.event_version = 1;
+  }
+}
+
+class StubAggregate extends AggregateRoot {
+  id: Uuid;
+  name: string;
+
+  action(name: string) {
+    this.name = name;
+    this.applyEvent(new StubEvent(this.id, this.name));
+  }
+
+  toJSON() {
+    return {
+      id: this.id.toString(),
+      name: this.name,
+    };
+  }
+}
+
+describe('DomainEventMediator Unit Tests', () => {
+  let mediator: DomainEventMediator;
+
+  beforeEach(() => {
+    mediator = new DomainEventMediator();
+  });
+
+  it('should publish handler', async () => {
+    expect.assertions(1);
+
+    mediator.register(StubEvent.name, (event: StubEvent) =>
+      expect(event.name).toBe('test'),
+    );
+
+    const aggregate = new StubAggregate();
+    aggregate.action('test');
+    await mediator.publish(aggregate);
+  });
+});
---
+++ src/core/@shared/domain/events/domain-event-mediator.ts
@@ -0,0 +1,21 @@
+import EventEmitter2 from 'eventemitter2';
+import { AggregateRoot } from '../aggregate-root';
+
+export class DomainEventMediator {
+  private eventEmitter: EventEmitter2;
+
+  constructor() {
+    this.eventEmitter = new EventEmitter2();
+  }
+
+  register(event: string, handler: any) {
+    this.eventEmitter.on(event, handler);
+  }
+
+  async publish(aggregateRoot: AggregateRoot) {
+    for (const event of aggregateRoot.events) {
+      const eventClassName = event.constructor.name;
+      await this.eventEmitter.emitAsync(eventClassName, event);
+    }
+  }
+}
